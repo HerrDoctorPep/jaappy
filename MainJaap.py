0f0e1c287617ca100be16aa1fdeadfea1eb86026
Herr doctor Pep <HerrDoctorPep@users.noreply.github.com>
2019-08-03 19:03:51 +0200

    Loop through all summary pages and write result to CSV

--- MainJaap.py
+++ MainJaap.py
@@ -8,10 +8,7 @@ By dr.Pep
 # Built-in/Generic imports
 
 # Libs
-import urllib.request, urllib.parse, urllib.error
-from bs4 import BeautifulSoup
-import re
-import ssl
+import time
 # import numpy as np
 import pandas as pd
 
@@ -29,6 +26,23 @@ __maintainer__ = 'dr.Pep'
 __email__ = 'python@vdlaan.eu'
 __status__ = 'In development'
 
-#
-# THIS IS WHERE THE FUN STARTS
-#
+# Set some constants
+
+URL = 'https://www.jaap.nl/koophuizen/zuid+holland/groot-rijnmond/rotterdam/50+-woonopp/' # The jaap.nl main page for scraping (Rotterdam, 50m²+)
+MAXPAGE = get_max_page(URL) # The number of summary pages to be scraped
+TODAY = time.strftime("%Y%m%d") # The current date in the standard format YYYYMMDD
+
+print('Website to be scraped:', URL)
+print('Number of pages:', MAXPAGE)
+print('Date:',TODAY)
+
+for p in range(MAXPAGE):
+    df_houses_read = read_summary_page(URL,p+1)
+    if p == 0:
+        df_houses_summary = df_houses_read
+    else:
+        df_houses_summary = pd.concat([df_houses_summary,df_houses_read],ignore_index=True, sort =False)
+     
+df_houses_summary.to_csv('houses_summary'+str(TODAY),index=False, header=True)
+
+print('All',len(df_houses_summary), 'houses have been written to file!')
