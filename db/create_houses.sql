188b396227138fd72cb976d806255cfdaf314eec
Herr doctor Pep <HerrDoctorPep@users.noreply.github.com>
2021-03-02 22:19:09 +0100

    Adjust database schema (multiple tables)

--- db/create_houses.sql
+++ db/create_houses.sql
@@ -1,41 +1,66 @@
 CREATE DATABASE IF NOT EXISTS houses_db;
 
-CREATE TABLE IF NOT EXISTS readings(
-    Reading_index INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Reading_date DATE,
+CREATE TABLE IF NOT EXISTS locations(
+    Index_ INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Date_ DATE,
+    ID_ VARCHAR(255),
     Address_ VARCHAR(255) NOT NULL,
-    Balkon_ BOOL DEFAULT FALSE,
-    Bijzonderheden_ VARCHAR(255),
+    Zip_ VARCHAR(255),
+    ZipCode_ VARCHAR(255),
+    Longitude_ FLOAT(9,8),
+    Latitude_ FLOAT(9,7)
+);
+
+CREATE TABLE IF NOT EXISTS attributes_num(
+    Index_ INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Date_ DATE,
+    ID_ VARCHAR(255),
     Bouwjaar_ INT,
-    Broker_ VARCHAR(255),
+    Inhoud_ INT,
+    Kamers_ INT,
+    Perceeloppervlakte_ INT,
+    Slaapkamers_ INT,
+    Woonoppervlakte_ INT
+);
+
+CREATE TABLE IF NOT EXISTS attributes_str(
+    Index_ INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Date_ DATE,
+    ID_ VARCHAR(255),
+    Balkon_ VARCHAR(255),
+    Bijzonderheden_ VARCHAR(255),
     Energielabel_(geschat)_ VARCHAR(255),
     Energieverbruik_(geschat)_ VARCHAR(255),
-    Garage_  BOOL DEFAULT FALSE,
-    Geplaatst_op_ DATE,
-    Huidige_vraagprijs_ VARCHAR(255),
-    ID_ VARCHAR(255),
-    Inhoud_ INT,
+    Garage_ VARCHAR(255),
     Isolatie_ VARCHAR(255),
-    Kamers_  INT,
     Keuken_ VARCHAR(255),
-    Long_description_ TEXT,
-    Oorspronkelijke_vraagprijs_ INT,
-    Perceeloppervlakte_ INT,
-    Pricetype_ VARCHAR(255),
-    Sanitaire_voorzieningen_ VARCHAR(255),
-    Short_description_ TEXT,
-    Slaapkamers_ INT,
-    Sold_ VARCHAR(255),
     Staat_onderhoud_ VARCHAR(255),
     Staat_schilderwerk_ VARCHAR(255),
     Tuin_ VARCHAR(255),
     Type_ VARCHAR(255),
     Uitzicht_ VARCHAR(255),
-    Verwarming_ VARCHAR(255),
-    Woonoppervlakte_ INT,
-    Zip_ VARCHAR(255),
-    ZipCode_ VARCHAR(255),
-    Longitude_ FLOAT(1,8),
-    Latitude_ FLOAT(2,7)
+    Verwarming_ VARCHAR(255)
 );
 
+CREATE TABLE IF NOT EXISTS attributes_txt(
+    Index_ INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Date_ DATE,
+    ID_ VARCHAR(255),
+    Long_description_ TEXT,
+    Short_description_ TEXT
+);
+
+CREATE TABLE IF NOT EXISTS salesinfo(
+    Index_ INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Date_ DATE,
+    ID_ VARCHAR(255),
+    Broker_ VARCHAR(255),
+    Geplaatst_op_ DATE,
+    Huidige_vraagprijs_ VARCHAR(255),
+    Oorspronkelijke_vraagprijs_ INT,
+    Pricetype_ VARCHAR(255),
+    Sold_ VARCHAR(255)
+);
+
+GRANT ALL PRIVILEGES ON houses_db.* TO 'xxx'@'localhost' IDENTIFIED BY 'xxxxxx';
+FLUSH PRIVILEGES;
